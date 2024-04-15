39056ec2a2e09949578efe6cc08bbf39d0ce7b77
erminio-ricciardi <erminio.ricciardi@gmail.com>
2024-04-15 10:16:28 +0200

    public website url update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const NEXT_PUBLIC_SITE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://herminius.eth.limo'
+    ? 'https://farcaster-portfolio-deck-frame.vercel.app/'
     : 'http://localhost:3001';
